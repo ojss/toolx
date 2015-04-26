be219f1236ad88551599804bef3b1fdbcad2ac30
Ojas Shirekar <ojas.shirekar@gmail.com>
2015-04-26 14:27:02 +0530

    Rolled back action bar colors

--- app/src/main/java/com/projectx/toolx/MainActivity.java
+++ app/src/main/java/com/projectx/toolx/MainActivity.java
@@ -1,8 +1,8 @@
 package com.projectx.toolx;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -52,7 +52,6 @@ public class MainActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
