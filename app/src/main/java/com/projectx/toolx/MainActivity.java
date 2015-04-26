e6a71be0c157c5544868e535991552c8e93cfc89
Ojas Shirekar <ojas.shirekar@gmail.com>
2015-04-26 13:38:53 +0530

    First Commit

--- app/src/main/java/com/projectx/toolx/MainActivity.java
+++ app/src/main/java/com/projectx/toolx/MainActivity.java
@@ -9,13 +9,11 @@ import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
     public void cListener(View view)
     {
         Intent intent = new Intent(getApplicationContext(), compass.class);
@@ -37,14 +35,12 @@ public class MainActivity extends ActionBarActivity {
         Intent intent = new Intent(getApplicationContext(), unit_converter.class);
         startActivity(intent);
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
